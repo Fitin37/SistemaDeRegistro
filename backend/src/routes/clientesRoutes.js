4b16bf3b924dd78d82502ce436e69596b10c0c77
20230300@ricaldone.edu.sv <20230300@ricaldone.edu.sv>
2025-09-08 18:25:16 -0600

    v6

--- backend/src/routes/clientesRoutes.js
+++ backend/src/routes/clientesRoutes.js
@@ -1,14 +1,13 @@
 import express from "express"
 import clienteCon from "../controller/clienteController.js"
 
-const router=express.Router();
+const router = express.Router();
 
-// En tu archivo de rutas
-router.post('/clientes', clienteCon.agregarCliente);
-router.get('/clientes', clienteCon.get);
-router.get('/clientes/:id', clienteCon.getClienteById);
-router.put('/clientes/:id', clienteCon.PutClientes);
-router.delete('/clientes/:id', clienteCon.deleteClientes);
-
+// Cambiar todas las rutas para quitar '/clientes'
+router.post('/', clienteCon.agregarCliente);       
+router.get('/', clienteCon.get);                    
+router.get('/:id', clienteCon.getClienteById);     
+router.put('/:id', clienteCon.PutClientes);         
+router.delete('/:id', clienteCon.deleteClientes);  
 
 export default router;
